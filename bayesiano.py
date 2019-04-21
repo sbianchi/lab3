e86d2482e712ec6755b6140e56b396ef4838eb91
Santiago Rodriguez <santi_1409@hotmail.com>
2019-04-20 22:30:52 -0300

    arreglo identacion

--- bayesiano.py
+++ bayesiano.py
@@ -42,9 +42,9 @@ def calculateClassProbabilities(summaries, inputVector):
     for classValue, classSummaries in summaries.items():
     	probabilities[classValue] = 1
     	for i in range(len(classSummaries)):
-    		mean, stdev = classSummaries[i]
-    		x = inputVector[i]
-    		probabilities[classValue] *= calculateProbability(x, mean, stdev)
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
     return probabilities
 
 def predict(summaries, inputVector):
